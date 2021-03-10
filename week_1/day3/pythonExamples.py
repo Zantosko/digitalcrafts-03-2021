1d45d00d9a0b35a7a274a216288d0abc91c5f9aa
Zach Antosko <zachary.antosko@gmail.com>
2021-03-10 10:24:38 -0600

    created my first program

---
+++ week_1/day3/pythonExamples.py
@@ -0,0 +1,38 @@
+# Variable
+# String === "", "word", "1", "functionName"
+greeting = "Hello World"
+
+# Integer === whole number, 1, 23, 435675, 1314
+classSize = 23
+
+# Float === 4.5, 4.5556666, 4.556567668788778899
+floatExample = 2.248
+
+# Boolean === True or False, True, False
+sumOfNumbers = 1 + 1
+
+# Name, assignment operator (=), value that I am assigning
+variableName = True
+
+# List, sequence of values followed by [value1, value2, etc]
+# [1, 2, 3]
+# ["strings", "string2", "blahblahblah"]
+# [False, True, True]
+# ["string", 1, 3.4, True, "string"]
+
+# Nesting
+# ["list", ["list"]]
+# Dictionary
+# Sequence of labeled values {key: value}
+# {"stringName": "StringValue"}
+# {"FavoriteColor": "Red"}
+
+
+# TRUTHY => look this up
+# FALSY => look this up
+
+# Will return True in the terminal
+# print(sumOfNumbers == 2)
+
+# Invoking a function ()
+print("Hello World")
