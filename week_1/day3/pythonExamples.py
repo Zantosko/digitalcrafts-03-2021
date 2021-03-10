b9c9f0313ccf83cdf520bf984383d8668cdf0f8d
Zach Antosko <zachary.antosko@gmail.com>
2021-03-10 11:35:18 -0600

    added more print examples

--- week_1/day3/pythonExamples.py
+++ week_1/day3/pythonExamples.py
@@ -4,18 +4,21 @@ greeting = "Hello World"
 
 # Integer === whole number, 1, 23, 435675, 1314
 classSize = 23
+dirPlusTeacher = 4
 
 # Float === 4.5, 4.5556666, 4.556567668788778899
 floatExample = 2.248
 
 # Boolean === True or False, True, False
-sumOfNumbers = 1 + 1
+sumOfTeachers = dirPlusTeacher + 1
+sumOfClass = dirPlusTeacher + classSize
 
 # Name, assignment operator (=), value that I am assigning
 variableName = True
 somethingToCallIt = ["students", 23, True]
 # This one will get printed to the console because its the reassigned value
 somethingToCallIt = {"helloPythonWorld": 23, "We are students": 24.5}
+print("The size of our class is", sumOfClass)
 
 # List, sequence of values followed by [value1, value2, etc]
 # [1, 2, 3]
@@ -38,8 +41,8 @@ somethingToCallIt = {"helloPythonWorld": 23, "We are students": 24.5}
 # print(sumOfNumbers == 2)
 
 # Invoking a function ()
-print("Hello World")
-print("My name is Zach")
-print(classSize)
-print(somethingToCallIt)
-variableNameThree = 3
+# print("Hello World")
+# print("My name is Zach")
+# print(classSize)
+# print(somethingToCallIt)
+# variableNameThree = 3
