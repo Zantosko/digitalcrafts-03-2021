a72f7292463a32e7314fb4942c14b027e7dad259
Zach Antosko <zachary.antosko@gmail.com>
2021-03-11 07:47:26 -0600

    commiting personal practice problems

---
+++ week_1/day3/personal_practice.py
@@ -0,0 +1,38 @@
+# Prints string in ALL CAPS
+# string = "hello"
+# print(string.upper())
+
+# Madlib
+# print("Please fill in the blank below")
+# print("___(name)___ likes coding!")
+
+# name = input("> ")
+# print(f"{name} likes coding!")
+
+# Day off the week
+# day = int(input("Day (0-6)? "))
+# while day <= 6:
+#     if day == 0:
+#         print("Sunday")
+#         break
+#     elif day == 1:
+#         print("Monday")
+#         break
+#     elif day == 2:
+#         print("Tuesday")
+#         break
+#     elif day == 3:
+#         print("Wednesday")
+#         break
+#     elif day == 4:
+#         print("Thursday")
+#         break
+#     elif day == 5:
+#         print("Friday")
+#         break
+#     elif day == 6:
+#         print("Saturday")
+#         break
+#     else:
+#         print("Invalid input, please try again.")
+#         day = int(input("Day (0-6)? "))
