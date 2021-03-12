d5a72f3cc9277c72768fd9fdbc7fc89ccb81e8d7
Zach Antosko <zachary.antosko@gmail.com>
2021-03-12 08:27:33 -0600

    finished refactored calculator app

--- week_1/day4/refactored_calculator.py
+++ week_1/day4/refactored_calculator.py
@@ -7,9 +7,13 @@ Input 3 - Represents the third number
 
 print("Welcome to the Calculator Application")
 print("Enter 'q' to quit at any time")
-# first_number = input("Please enter a number > ")
-# operator = input("Please select an operator (+, -, *, /) > ")
-# second_number = input("Please enter another number > ")
+print(
+    """Please enter 3 values 
+The first value should be a number
+The second value should be a operator (+, -, *, /)
+The third value should be another number
+After this you will be prompted again to press '=' for the solution"""
+)
 
 
 def quit():
@@ -32,41 +36,56 @@ def division(a, b):
     return a / b
 
 
-# Make quit function
-operation = list(map(str, input("> ").split()))
+operation = list(map(str, input("Enter 3 values here > ").split()))
 length = len(operation)
 
-if operation == "q":
-    quit()
-else:
-    pass
 
-while True:
-    if length > 3:
-        print("No more than 3 arguments")
-        operation = list(map(str, input("> ").split()))
-        length = len(operation)
-    else:
-        break
+while operation[0] != "q":
 
+    while True:
+        if length > 3:
+            print("No more than 3 arguments")
+            operation = list(map(str, input("> ").split()))
+            length = len(operation)
+        else:
+            break
 
-while True:
-    solution = None
-    if operation[1] == "+":
-        solution = addition(int(operation[0]), int(operation[2]))
-        print(f"{operation[0]} + {operation[2]} = {solution}")
-        exit(0)
-    elif operation[1] == "-":
-        solution = subtraction(int(operation[0]), int(operation[2]))
-        print(f"{operation[0]} - {operation[2]} = {solution}")
-        exit(0)
-    elif operation[1] == "*":
-        solution = multiplication(int(operation[0]), int(operation[2]))
-        print(f"{operation[0]} * {operation[2]} = {solution}")
-        exit(0)
-    elif operation[1] == "/":
-        solution = division(int(operation[0]), int(operation[2]))
-        print(f"{operation[0]} / {operation[2]} = {solution}")
-        exit(0)
-    else:
-        print("Something went wrong, please restart.")
+    while True:
+        solution = None
+        if operation[1] == "+":
+            solution = addition(int(operation[0]), int(operation[2]))
+            input_2 = input("Press '=' for solution > ")
+            if input_2 == "=":
+                print(f"{operation[0]} + {operation[2]} = {solution}")
+                exit(0)
+            else:
+                print("Invalid input")
+        elif operation[1] == "-":
+            solution = subtraction(int(operation[0]), int(operation[2]))
+            input_2 = input("Press '=' for solution > ")
+            if input_2 == "=":
+                print(f"{operation[0]} - {operation[2]} = {solution}")
+                exit(0)
+            else:
+                print("Invalid input")
+        elif operation[1] == "*":
+            solution = multiplication(int(operation[0]), int(operation[2]))
+            input_2 = input("Press '=' for solution > ")
+            if input_2 == "=":
+                print(f"{operation[0]} * {operation[2]} = {solution}")
+                exit(0)
+            else:
+                print("Invalid input")
+        elif operation[1] == "/":
+            solution = division(int(operation[0]), int(operation[2]))
+            input_2 = input("Press '=' for solution > ")
+            if input_2 == "=":
+                print(f"{operation[0]} / {operation[2]} = {solution}")
+                exit(0)
+            else:
+                print("Invalid input")
+        elif operation[0].lower() == "q":
+            quit()
+        else:
+            print("Something went wrong, please restart.")
+            quit()
