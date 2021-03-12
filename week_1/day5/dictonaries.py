47e07b1d871bfdd9b7f30e7465fbfdfc7379428d
Zach Antosko <zachary.antosko@gmail.com>
2021-03-12 15:18:20 -0600

    committing notes from class

---
+++ week_1/day5/dictonaries.py
@@ -0,0 +1,11 @@
+# Ways to work with Dictonaries
+# Creates empty dictionary
+my_dict = {}
+# Takes first input from user
+task = input()
+# Takes second input from user
+priority = input()
+
+# Store values in the empty dictionary
+my_dict["task"] = task
+my_dict["priority"] = priority
