5a43eea0a53de2dc32d20d160d1f035ddcebaf58
Zach Antosko <zachary.antosko@gmail.com>
2021-03-17 09:38:05 -0500

    inital commit for day3 file

---
+++ week_2/day3/inheritanceClasses.py
@@ -0,0 +1,9 @@
+# Inheritance Practice
+class Currency:
+    def __init__(self, name, homeNationCurrency, price):
+        self.name = name,
+        self.homeNationCurrency = homeNationCurrency,
+        self.price = price
+
+
+yen = Currency("Yen", "Japan", 20)
