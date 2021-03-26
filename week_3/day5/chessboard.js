efd10b93a987ffd2a9cef3a27784d6b1e3a0245c
Zach Antosko <zachary.antosko@gmail.com>
2021-03-26 16:27:38 -0500

    work from yesterday and today

---
+++ week_3/day5/chessboard.js
@@ -0,0 +1,18 @@
+let chessboard = [];
+
+// for (let white of chessboard) {
+//   white = "white";
+//   chessboard.push(white);
+//   console.log(chessboard)
+// }
+
+// for (let white of range(1, 9)) {
+//   white = " "
+//   chessboard.push(white)
+// }
+
+for (let i = 0; i <= 8; i++) {
+  i = " "
+}
+
+console.log(chessboard)
---
+++ week_3/day5/debug.js
@@ -0,0 +1,7 @@
+const title = document.getElementById("Title");
+const img1 = document.getElementById("img1");
+
+img1.addEventListener("click", () => {
+  img1.width = 400;
+})
+
