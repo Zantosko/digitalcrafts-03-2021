160eb962aab76cb07733aa2eecb069d05ab5d77c
Zach Antosko <zachary.antosko@gmail.com>
2021-04-05 20:57:34 -0500

    committing finished assignment

--- week_5/day1/assignments/weathersite.js
+++ week_5/day1/assignments/weathersite.js
@@ -1,30 +1,52 @@
 const API = "eed38ad6a02273431c1a13c74cf193f9"
+const body = document.querySelector("body")
 const zipCode = document.querySelector(".zip");
 const submitBtn = document.querySelector(".btn")
 let card = document.querySelector(".card-2")
 
 
-const getWeather = async () => {
-  console.log(zipCode.value)
-  const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode.value},us&units=imperial&appid=${API}`)
-  console.log(data)
+let currentTime = new Date().getHours();
+if (body) {
+    if (7 <= currentTime && currentTime < 20) {
+        body.setAttribute("class","day")
+      }
+      else {
+        body.setAttribute("class","night")
+        submitBtn.style.backgroundColor = "#C26DBC"
+    }
+}
 
-  const convertToJSON = await data.json()
-  console.log(convertToJSON)
-  
-  card.innerHTML = `
-    <h1>${convertToJSON.name}</h1>
-    <div class="grid">
-      <div class="grid-left">
-        <p>${convertToJSON.weather[0].description}</p>
-        <p class="temp">${convertToJSON.main.temp} &degF</p>
-      </div>
-      <div class="grid-right">
-        <img src="http://openweathermap.org/img/w/${convertToJSON.weather[0].icon}.png">
-      </div>
-    </div>
-  `
-  card.style.visibility = "visible"
+
+const getWeather = async () => {
+  if (zipCode.value.length !== 5) {
+    alert("Invalid Entry! The entry must contain exactly 5 digits!")
+    zipCode.value = "";
+  } else {
+      try {
+        const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode.value},us&units=imperial&appid=${API}`)
+      
+        const convertToJSON = await data.json()
+        
+        card.innerHTML = `
+          <h1>${convertToJSON.name}</h1>
+          <div class="grid">
+            <div class="grid-left">
+              <p>${convertToJSON.weather[0].description}</p>
+              <p class="temp">${convertToJSON.main.temp} &degF</p>
+            </div>
+            <div class="grid-right">
+              <img src="http://openweathermap.org/img/w/${convertToJSON.weather[0].icon}.png">
+            </div>
+          </div>
+        `
+        card.style.visibility = "visible"
+      } catch (err) {
+        err = "Invalid Entry! The ZIP code entered does not exist!"
+        alert(err)
+        location.reload();
+      }
+  }
 }
 
 submitBtn.addEventListener("click", getWeather)
+
