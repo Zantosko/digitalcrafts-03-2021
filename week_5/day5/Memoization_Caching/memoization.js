3beedcce16d77e31df1493d24ec757098f528952
Zach Antosko <zachary.antosko@gmail.com>
2021-04-10 13:52:13 -0500

    more memoization practice

--- week_5/day5/Memoization_Caching/memoization.js
+++ week_5/day5/Memoization_Caching/memoization.js
@@ -1,7 +1,8 @@
 //* Memoization
 
 //? Quadratic Time Operation
-const isUninque = (arr) => {
+// This solution is less optimal because nested for-loops have a time complexity of O(n^2). This drastically increases time relative to the amount of operations performed (Quadratic Correlation). 
+let isUninque = (arr) => {
   let result = true;
 
   for (let i = 0; i < arr.length; i++) {
@@ -13,6 +14,28 @@ const isUninque = (arr) => {
       }
     }
   }
+  return result;
+}
+
+// returns true
+console.log(isUninque([1,2,3]))
+// returns false
+console.log(isUninque([1,1,3]))
+
+//! Improved with Caching
+// Using this solution changed the time complexity to O(n) giving us huge gains in performance. Time has a Linear Correlation relative to the number of operations taking place.
+isUninque = (arr) => {
+  const cache = {};
+  let result = true;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (cache[arr[i]]) {
+      result = false;
+    } else {
+      cache[arr[i]] = true;
+    }
+  }
+
   return result;
 }
 
---
+++ week_5/day5/Memoization_Caching/memoization2.js
@@ -0,0 +1,35 @@
+// Memoization is a type of caching. If you are caching the result of a function, it's called memoization. Caching makes a trade-off between time complexity and space complexity. It's faster, but it does take up more space in memory.
+
+
+//? Sorting
+const uniqSort = (arr) => {
+
+  return arr.sort((a, b) => a - b);
+}
+
+
+//? Memoization Exercise
+// Write a function, times10 that takes an argument "n" and multiples "n" times 10.
+// const times10 = (n) => n * 10;
+
+// console.log("times10 returns:", times10(9));
+
+const cache = {};
+
+const times10 = (n) => n * 10;
+
+const memoTimes10 = (n) => {
+  if (n in cache) {
+    console.log("Fetching from cache", n)
+    return cache[n];
+  } else {
+    console.log("calculating result");
+    cache[n] = times10(n);
+    return cache[n];
+  }
+}
+// Result: Fetching from cache 9
+console.log('Calculated value of 9:', memoTimes10(9));    // calculated
+
+// Fecthing from cache 9
+console.log('Cached Value of 9', memoTimes10(9));    // cached
