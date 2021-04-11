956c1ef641beae480e3f0ce0cec171f98209fc96
Zach Antosko <zachary.antosko@gmail.com>
2021-04-11 10:48:12 -0500

    more array method practice

--- week_5/day5/Array-Methods/JS-higher-order-functions_arrays.js
+++ week_5/day5/Array-Methods/JS-higher-order-functions_arrays.js
@@ -14,7 +14,7 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 //* Higher Order Functions & Arrays
 
-//? forEach - Used to loop through an array (similar to a for-loop). It doesn't return anything back to you, but its a cleaner way to iterate over arrays. forEach takes in a synchronous callback function(not asynchronous).
+//? forEach() - Used to loop through an array (similar to a for-loop). It doesn't return anything back to you, but its a cleaner way to iterate over arrays. forEach takes in a synchronous callback function(not asynchronous).
 
 // Old way to do it
 // for (let i = 0; i < companies.length; i++) {
@@ -27,9 +27,10 @@ companies.forEach((company) => {
 });
 
 
-//? filter - It allows you to filter things out from the array. With this method you don't have to create a new array and push to it, the filter() method takes care of that for you.
 
-//todo Example 1
+//? filter() - It allows you to filter things out from the array. With this method you don't have to create a new array and push to it, the filter() method takes care of that for you.
+
+//todo Example 1 - Checks for ages greater than 21
 // Old way
 // let canDrink = [];
 // for (let i = 0; i < ages.length; i++) {
@@ -52,9 +53,68 @@ const canDrink = ages.filter(age => age >= 21);
 
 console.log(canDrink)
 
-//todo Example 2
+//todo Example 2 - Checks for companies with "Retail" category
+
+// ES5 Version
+// const retailCompanies = companies.filter(function(company) {
+//   if(company.category === "Retail") {
+//     return true;
+//   }
+// });
+
+// ES6 Version
+const retailCompanies = companies.filter(company => company.category === "Retail");
+
+//todo Example 3 - Checks for companies that started in the 80s
+
+const eightiesCompanies = companies.filter(company => (company.start >= 1980 && company.start < 1990));
+
+console.log(eightiesCompanies)
 
 
-//? map - 
-//? sort
-//? reduce
+//todo Example 4 - Checks for companies that lasted 10 years or more
+
+const lastedTenYears = companies.filter(company => (company.end - company.start >= 10));
+
+console.log(lastedTenYears)
+
+
+
+//? map() - Creates new arrays from a current array.
+
+//todo Example 1 - Creates an array of company names
+
+const companyNames = companies.map(company => company.name);
+console.log(companyNames);
+
+//todo Example 2 - Creates an array with company names along with start and end dates
+
+const testMap = companies.map(company => {
+  return `${company.name} [${company.start} - ${company.end}]`
+});
+console.log(testMap);
+
+//todo Example 3 - Creates an array with square root of each age
+
+const agesSquare = ages.map(age => Math.sqrt(age));
+console.log(agesSquare)
+
+//todo Example 4 - Creates an array with square root of each age and then multiplies it by 2
+
+const agesSquareTimesTwo = ages
+  .map(age => Math.sqrt(age))
+  .map(age => age * 2)
+console.log(agesSquareTimesTwo)
+
+
+//? sort() - Can organize array items by alphabetical or chronological order.
+
+const sortedCompanies = companies.sort(function(c1, c2) {
+  if(c1.start > c2.start) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+//? reduce()
