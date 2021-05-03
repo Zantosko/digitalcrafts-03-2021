b4efdbe03d7e773e0ad6f850d24d15f7c75ea8ff
Zach Antosko <zachary.antosko@gmail.com>
2021-05-03 08:16:18 -0500

    fixed bugs on the site

--- week_8/day5/practice-login/server/index.js
+++ week_8/day5/practice-login/server/index.js
@@ -1,6 +1,6 @@
-if(process.env.NODE_ENV !== "production") {
-  require("dotenv").config()
-}
+// if(process.env.NODE_ENV !== "production") {
+// }
+require("dotenv").config()
 
 const express = require("express");
 const cors = require("cors");
@@ -26,7 +26,7 @@ const users = [];
 
 // Middleware
 // requires static files
-app.use("../public",express.static("css"));
+app.use(express.static("../public"));
 app.use(express.json());
 app.use(cors());
 app.use(flash());
@@ -46,6 +46,7 @@ app.set("view engine", "html");
 // access our form information inside of our request body
 app.use(express.urlencoded({ extended: false }));
 
+
 // CUSTOM MIDDLEWARE
 function checkAuthenticated(req,res,next) {
   if(req.isAuthenticated()) {
@@ -56,42 +57,52 @@ function checkAuthenticated(req,res,next) {
 
 function checkIfUserIsLoggedIn(req,res,next) {
   if(req.isAuthenticated()) {
-    return next()
+    return res.redirect("/") 
   }
-  res.redirect("/login")
+
+  next()
 }
 
 app.get("/", checkAuthenticated,(req,res) => {
-  res.render("home");
+  res.render("home", { locals: { name: req.user.name } });
 });
 
 app.get("/login", checkIfUserIsLoggedIn,(req,res) => {
   res.render("login");
 });
 
-app.post("/login", passport.authenticate("local"), {
+app.post("/login", passport.authenticate("local", {
   successRedirect: "/",
   failureRedirect: "/login",
   failureFlash: true
-})
+}));
 
-app.get("/register", (req,res) => {
+app.get("/register", checkIfUserIsLoggedIn,(req,res) => {
   res.render("register");
 });
 
-app.post("/register", checkIfUserIsLoggedIn,async (req,res) => {
+app.post("/register", async (req,res) => {
   try {
     const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash("123");
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    console.log(req.body);
-    console.log(salt);
-    console.log(hashedPassword)
-    res.status(200).send("Ok")
+    const user = {
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    }
+    users.push(user)
+    res.status(200).redirect("/login");
   } catch (error) {
-    console.error(error.message);
+    res.status(401).redirect("/register")
   }
+});
+app.post("/logout", (req,res) => {
+  req.logOut();
+  res.redirect("/login");
 })
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
