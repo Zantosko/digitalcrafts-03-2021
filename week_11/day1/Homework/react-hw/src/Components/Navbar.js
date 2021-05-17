b31c9ca4cb39de16e56a3b4569671740921aab9b
Zach Antosko <zachary.antosko@gmail.com>
2021-05-17 18:34:02 -0500

    finished react hw

--- week_11/day1/Homework/react-hw/src/Components/Navbar.js
+++ week_11/day1/Homework/react-hw/src/Components/Navbar.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Dots from './assets/dots.png'
+import Account from './assets/Account.png'
 
 export default class Navbar extends Component {
   render() {
@@ -11,9 +13,11 @@ export default class Navbar extends Component {
           </ul>
         </div>
         <div className="right-nav">
-        <ul className="nav-container">
+          <ul className="nav-container">
             <li>Gmail</li>
             <li>Images</li>
+            <img src={Dots} height="38" width="38"/>
+            <img src={Account} height="38" width="40"/>
           </ul>
         </div>
       </div>
