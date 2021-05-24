f2be000ece77dcb9012cbe3ed7ef9e81f315e37c
Zach Antosko <zachary.antosko@gmail.com>
2021-05-24 11:19:38 -0500

    added to joke app

--- week_12/day1/examples/react-week12/src/components/JokeContainer.jsx
+++ week_12/day1/examples/react-week12/src/components/JokeContainer.jsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect }  from 'react'
 import Joke from './Joke'
 
 export default function JokeContainer() {
-
   const [joke, setJoke] = useState("");
+  const [name, setName] = useState("Zach");
 
   useEffect(() => {
-    console.log("Hello World")
-  });
+    getJoke();
+  },[name]);
 
   //* componentDidMount
   //? This triggers everytime the component is mounted.
@@ -16,7 +16,9 @@ export default function JokeContainer() {
   //* componentDidUpdate
   //? The array at the end is known as the Dependency Array.
   //? This will trigger one time only, after the component is mounted.
+  //? If you do an API call that sets state, don't do it without the dependency array. Without the dependency array you'll be caught in an infinite loop.
   // useEffect(() => {}, [])
+
   //? This will trigger everytime the component is mounted and everytime the value in the array is updated.
   // useEffect(() => {}, ["value"])
 
@@ -24,7 +26,15 @@ export default function JokeContainer() {
   // useEffect(())
 
   const getJoke = async () => {
+    const fetchJoke = await fetch("https://icanhazdadjoke.com", {
+      headers: {
+        "Accept": "application/json"
+      }
+    });
+    const jsonJoke = await fetchJoke.json()
+    console.log(jsonJoke)
 
+    setJoke(jsonJoke)
   }
 
   return (
