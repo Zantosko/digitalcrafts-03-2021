45b008bb81cf5b25c1959ada83dd5d87ae144a1c
Zach Antosko <zachary.antosko@gmail.com>
2021-05-25 13:44:59 -0500

    finished in class react-router assignment

--- week_12/day2/examples/react-router/src/App.js
+++ week_12/day2/examples/react-router/src/App.js
@@ -2,15 +2,33 @@ import './App.css';
 import AboutMe from './components/AboutMe';
 import Blog from './components/Blog';
 import Home from './components/Home';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <AboutMe />
-        <Blog />
-        <Home />
+        <div>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/blog">Blog</Link>
+          </nav>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/aboutme">
+            <AboutMe />
+          </Route>
+          <Route exact path="/blog">
+            <Blog />
+          </Route>
+          <Route exact path="*">
+            <h1>404 Page Does Not Exist!</h1>
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
